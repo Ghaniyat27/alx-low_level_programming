9b75544c4c2a8b376f77890b419a10a76a497bca
Ghaniyat27 <ganiyat4real1@gmail.com>
2023-07-30 02:34:53 -0700

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,7 @@ void first(void) __attribute__ ((constructor));
  * function is executed
  */
 void first(void)
+
 {
 	printf("You're a beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
