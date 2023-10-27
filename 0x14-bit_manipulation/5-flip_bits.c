8b681be736d9577ecf6fe33397c900b3a11f0751
Ghaniyat27 <ganiyat4real1@gmail.com>
2023-10-27 09:04:47 -0700

    My Bit Manipulation task

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -4,20 +4,20 @@
  * flip_bits - figures out how many bits need to be changed
  * to get from one number to another
  * @n: first number
- * @s: second number
+ * @m: second number
  *
  * Return: number of bit's to be changed
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int s)
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int a, countbit = 0;
 	unsigned long int existing;
-	unsigned long int exclusive = n ^ s;
+	unsigned long int exclusive = n ^ m;
 
 	for (a = 63; a >= 0; a--)
 	{
 		existing = exclusive >> a;
-		if (current & 1)
+		if (existing & 1)
 			countbit++;
 	}
 	return (countbit);
